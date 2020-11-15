59d6f6a799b5d49718f600d9e685c4bff7bf0ff5
DeaconMofoJones <35055693+DeaconMofoJones@users.noreply.github.com>
2020-11-14 22:47:54 -0800

    Created index.js, which is our server file

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+//Allows us to render ejs files instead of html files.
+//(ejs files are pretty much the same as html files. The only difference is that they are inclusive of javascript.)
+app.set("view-engine", "ejs");
+
+//Tells our app to look inside the public directory for files, like our images.
+app.use(express.static(__dirname + '/public'));
+
+//ROUTES
+
+//root route
+app.get('/', (req, res) => {
+    res.render("index.ejs");
+})
+
+//Server starts here with a port of 3000
+app.listen(3000);
